0b0763bde9f58929f08a41cdbfd48f371156ca8a
Paulo <pauloricardo.leite@hotmail.com>
2023-09-26 22:24:45 -0300

    hide pass from inserted user

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -3,12 +3,13 @@ import user from '../../models/userModel.js'
 const createUser = async (req, res) => {
     try {
         const [result] = await user.create(req.body)
-
         if(result.affectedRows === 1) {
+            const newUser = req.body
+            delete newUser.pass
             res.status(201).json({success: 'User created',
                 user: {
                     id: result.insertId,
-                    ...req.body
+                    ...newUser
                 }
             })
         }
